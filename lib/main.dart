7a41a877601412182ae983842f486bde69afadc5
anangfatkur <anangfatkurrr@gmail.com>
2021-12-10 10:31:20 +0700

    pengunaan get put, find dan lazy put

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final myC = Get.put(MyController());
+  // final myC = Get.put(MyController());
+  //YOUTUBE Penggunaan lazyput
+  final myC = Get.lazyPut(() => MyController(), tag: 'tag-text', fenix: true);
   MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -23,13 +25,53 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final myC = Get.put(
+    MyController(),
+    permanent: true,
+    tag: 'tag-count',
+  );
+
   // final MyController myC = Get.find();
-  final myC = Get.find<MyController>();
+  // final myC = Get.find<MyController>();
   HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => Get.to(() => CountPage),
+            icon: const Icon(Icons.alarm),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "${myC.count}",
+          style: const TextStyle(fontSize: 35),
+        ),
+      ),
+    );
+  }
+}
+
+class CountPage extends StatelessWidget {
+  // final MyController myC = Get.find();
+  final myC = Get.find<MyController>(tag: 'tag-count');
+  CountPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => Get.to(CountPage),
+            icon: const Icon(Icons.alarm),
+          ),
+        ],
+      ),
       body: Center(
         child: Obx(
           () => Text(
@@ -43,8 +85,40 @@ class HomePage extends StatelessWidget {
   }
 }
 
+class TextPage extends StatelessWidget {
+  // final textC = Get.put(
+  //   MyController(),
+  //   permanent: false,
+  //   tag: 'tag-text',
+  // );
+  final myC = Get.find<MyController>(tag: 'tag-text');
+
+  TextPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => Get.to(CountPage),
+            icon: const Icon(Icons.alarm),
+          ),
+        ],
+      ),
+      body: Center(
+        child: TextField(
+          controller: myC.textController,
+        ),
+      ),
+    );
+  }
+}
+
 class MyController extends GetxController {
   var count = 0.obs;
+
+  var textController = TextEditingController();
 
   void add() => count++;
 }
